95996b5e7e69635f01321de88680892af9c5cc7e
Ben Baumann <ben@thisisbam.com>
2017-12-30 20:31:49 -0500

    added documentation the readme & updated most of the routes, did not add logic

--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,60 @@ const express = require("express");
 const api = express.Router();
 // const db = require('../models/index');
 
-api.get("/api/login", (req, res)=>{
+
+// get all events
+api.get("/api/events", (req, res)=>{
 
 });
 
+// create an event
+api.post("/api/events", (req, res)=>{
+
+});
+
+// udpate an event
+api.put("/api/events/:id", (req, res)=>{
+
+});
+
+// delete an event
+api.delete("/api/events/:id", (req, res)=>{
+
+});
+
+// get all users
 api.get("/api/users", (req, res)=>{
   
 });
 
+// create a user
 api.post("/api/users", (req, res)=>{
 
 });
 
+// edit a user
+api.put("/api/users/:id", (req, res)=>{
+
+});
+
+// delete a user
+api.delete("/api/users/:id", (req, res)=>{
+
+});
+
+// try to login an admin
+api.post("/api/admin/login", (req, res)=>{
+
+});
+
+// try to login a user
+api.post("/api/volunteer/login", (req, res)=>{
+
+});
+
+// add a volunteer to an event
 api.post("/api/staging", (req, res)=>{
-    // this is where we'll post users volunteering for certain events
+    
 
 });
 
