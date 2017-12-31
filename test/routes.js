bc56253ee99fbbad7ac82c3ccad9e7a733df1954
Louise Hayes <louise@deewhy.ie>
2017-12-30 20:52:36 -0500

    initial unit testing config

---
+++ test/routes.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+// var utils = require('../app/utilities/utils.js');
+// var friends = require('../app/data/friends.js');
+
+describe('/api/route test', function () {
+    beforeEach(function () {
+
+
+    });
+
+    it('should return a value of 200 if api/route inserted sucessfully ', function () {
+        var var1 = 2;
+        console.log("testing api/route");
+
+        assert.equal(var1, '2');
+    });
+
+});
