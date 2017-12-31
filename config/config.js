d02a9f0a324b4e4b46d567c6a9f475b6205e91ff
Ben Baumann <ben@thisisbam.com>
2017-12-30 21:11:51 -0500

    added db setup instructions

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": `${process.env.DB_USER}`,
     "password": `${process.env.DB_PASS}`,
-    "database": `${process.env.DB}`,
+    "database": "event_monster_db",
     "host": `${process.env.DB_HOST}`,
     "dialect": "mysql"
   },
