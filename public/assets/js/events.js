2fddf8da446dd948667c99d8ef22beb5be3d48a5
James Martineau <James.Martineau@libertymutual.com>
2018-01-16 21:03:44 -0500

    delete and edit events complete

--- public/assets/js/events.js
+++ public/assets/js/events.js
@@ -1,7 +1,19 @@
-$(document).ready(()=>{
+var saveType;
+var eventID;
+
+$(document).ready(() => {
     $('#newEvent').modal();
 
-    $('.datepicker').pickadate({
+    var $startDatePicker = $('#start_date').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+
+    var $endDatePicker = $('#end_date').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year,
         today: 'Today',
@@ -13,7 +25,7 @@ $(document).ready(()=>{
     $('.timepicker').pickatime({
         default: 'now', // Set default time: 'now', '1:30AM', '16:30'
         fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
-        twelvehour: false, // Use AM/PM or 24-hour format
+        twelvehour: true, // Use AM/PM or 24-hour format
         donetext: 'OK', // text for done-button
         cleartext: 'Clear', // text for clear-button
         canceltext: 'Cancel', // Text for cancel-button
@@ -21,78 +33,100 @@ $(document).ready(()=>{
         ampmclickable: true, // make AM PM clickable
         aftershow: function () {} //Function for after opening timepicker
     });
-});
 
-$('#createEvent').on('click', function(){
-
-    var datetimerange = $("#datetime").val();
-
-    const event = {};
-    event.event_name = $('#event_name').val().trim();
-    event.start_date = $('#start_date').val();
-    event.start_time = $('#start_time').val();
-    event.end_date = $('#end_date').val();
-    event.end_time = $('#end_time').val();
-
-    console.log(event);
+    saveType = "POST";
 
 
-    $.ajax({
-            method: "POST",
-            url: "/api/events",
-            data: event
-        })
-        .done(function (msg) {
-            event.event_name = $('#event_name').val("");
-            event.start_date = $('#start_date').val("");
-            event.start_time = $('#start_time').val("");
-            event.end_date = $('#end_date').val("");
-            event.end_time = $('#end_time').val("");
 
-        });
-    $("#events-table tbody").append(
-        `<tr>
-        <td>${event.event_name}</td>
-        <td>${event.start_date}</td>
-        <td>${event.start_time}</td>
-        <td>${event.end_date}</td>
-        <td>${event.end_time}</td>
-        </tr>`
-    )
+    $('#saveEvent').on('click', function () {
 
-    Materialize.toast('Event Saved!', 4000)
-    console.log(msg);
+        var datetimerange = $("#datetime").val();
 
-});
+        const event = {};
+        event.event_name = $('#event_name').val().trim();
+        event.start_date = $('#start_date').val();
+        event.start_time = $('#start_time').val();
+        event.end_date = $('#end_date').val();
+        event.end_time = $('#end_time').val();
+        //this will only set if it's a PUT event
+        if (eventID) {
+            event.id = eventID;
+        }
+
+        console.log(event);
 
 
-  
+        $.ajax({
+                method: saveType, //POST or PUT
+                url: "/api/events",
+                data: event
+            })
+            .done(function (msg) {
+                event.event_name = $('#event_name').val("");
+                event.start_date = $('#start_date').val("");
+                event.start_time = $('#start_time').val("");
+                event.end_date = $('#end_date').val("");
+                event.end_time = $('#end_time').val("");
+
+                // Materialize.toast('Event Saved!', 4000)
+
+                window.location.reload();
+
+            });
 
 
-//EDIT EVENT
-$(".edit-event").on('click', function () {
-    var id = $(this).data("id");
 
-    $.ajax({
-            method: "GET",
-            url: "/api/events/" + id
-        })
-        .done(function (event) {
-            console.log("EVENT INFO\n\n" + JSON.stringify(event));
-        });
-});
+    });
 
-//DELETE EVENT
-$(".delete-event").on('click', function () {
-    var id = $(this).data("id");
 
-    $.ajax({
-            method: "DELETE",
-            url: "/api/events/" + id
-        })
-        .done(function (event) {
-            window.location.reload();
-            console.log("EVENT INFO\n\n" + JSON.stringify(event));
-        });
+    //EDIT EVENT
+    $(".edit-event").on('click', function () {
+        var id = $(this).data("id");
 
+        $.ajax({
+                method: "GET",
+                url: "/api/events/" + id
+            })
+            .done(function (event) {
+                console.log("EVENT INFO\n\n" + JSON.stringify(event));
+
+                var startPicker = $startDatePicker.pickadate('picker');
+                var endPicker = $endDatePicker.pickadate('picker');
+
+                // Using a string along with the parsing format (defaults to `format` option).
+                startPicker.set('select', event.start_date.split("T")[0], {
+                    format: 'yyyy-mm-dd'
+                });
+
+                endPicker.set('select', event.end_date.split("T")[0], {
+                    format: 'yyyy-mm-dd'
+                });
+
+                $('#event_name').val(event.event_name);
+                $('#start_time').val(event.start_time);
+                $('#end_time').val(event.end_time);
+
+                saveType = "PUT";
+
+                //set global to allow for PUT save event
+                eventID = event.id;
+
+                $('#newEvent').modal('open');
+            });
+    });
+
+    //DELETE EVENT
+    $(".delete-event").on('click', function () {
+        var id = $(this).data("id");
+
+        $.ajax({
+                method: "DELETE",
+                url: "/api/events/" + id
+            })
+            .done(function (event) {
+                window.location.reload();
+                console.log("EVENT INFO\n\n" + JSON.stringify(event));
+            });
+
+    });
 });
