1e47029b467f444f793efe8f37f0394701ee2498
Benjamin Baumann <benjaminbaumann@Bens-iMac.local>
2018-01-19 16:56:13 -0500

    db testing

--- models/staging.js
+++ models/staging.js
@@ -1,14 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Staging = sequelize.define('Staging', {
-    event_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+  var Staging = sequelize.define('Staging', {}, {
   });
   return Staging;
 };
