29ee2389c5d690b2be608a7b43129a86760a7218
Louise Hayes <louise@deewhy.ie>
2018-01-20 01:05:25 -0500

    user role select and activate user toggle

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $('select').material_select();
+    // Materialize.updateTextFields();
 });
 
 //when create user button is clicked
@@ -13,12 +15,23 @@ $("#create-user").on("click", function (user) {
     // populate user data captured in the modal from the partials users.create.handlebars 
     user.first_name = $('#first_name').val().trim();
     user.last_name = $('#last_name').val().trim();
-    user.email = $('#email').val().trim();
-    user.password = "";//obtain this from jwtauth cookies on server side in api.js /api/users
-    user.roleID = 1;//default to volunteer role
-    user.active = 0;
+    // user.email = $('#email').val().trim();
+    user.email = ""; //obtain this from jwtauth cookies on server side in api.js /api/users
+    user.password = ""; //obtain this from jwtauth cookies on server side in api.js /api/users
+    var roleID = $('.select-dropdown').val().trim();
+    if (roleID === "Admin") {
+        user.roleID = 2;
+    } else {
+        user.roleID = 1;
+    }
+    // user.roleID = 1;//1 to volunteer role 2 Admin Role
+    var active = $('#switch').val().trim();
+    if (active === "on") {
+        user.active = true;
+    } else {
+        user.active = false;
+    }
 
-    
     $.ajax({
         method: 'POST',
         url: "/api/users",
