74b58425c2a6a112903bf01db0ec8a7b6b8ab154
Benjamin Baumann <benjaminbaumann@Bens-iMac.local>
2018-01-20 19:11:51 -0500

    capitalized model in migrations

--- migrations/20180120213318-add-image-to-users.js
+++ migrations/20180120213318-add-image-to-users.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.addColumn('users', 'imgUrl', Sequelize.STRING );
+      return queryInterface.addColumn('Users', 'imgUrl', Sequelize.STRING );
   },
 
   down: (queryInterface, Sequelize) => {
-    queryInterface.removeColumn('users', 'imgUrl');
+    queryInterface.removeColumn('Users', 'imgUrl');
   }
 };
